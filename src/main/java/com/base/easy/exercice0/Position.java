2f6e07d8b37292d08dd993ea0ad8e5a5727a0790
xinri <xinri@xinri.familyds.net>
2019-04-18 19:07:20 +0200

    add exercice 0 in easy because is the easiest one. So that we can go into exercice 1 easily

---
+++ src/main/java/com/base/easy/exercice0/Position.java
@@ -0,0 +1,22 @@
+package com.base.easy.exercice0;
+
+import java.util.Objects;
+
+public class Position {
+
+  private final int x;
+  private final int y;
+
+  public Position(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public int getX() {
+    return x;
+  }
+}
