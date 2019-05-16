7b40350b6141bd8280d773db55a04883977e2878
xinri <xinri@xinri.familyds.net>
2019-05-16 13:42:10 +0200

    change exercice 1 into a similar exercice than the 0 with hashset

---
+++ src/main/java/com/base/easy/exercice1/PositionImproved.java
@@ -0,0 +1,20 @@
+package com.base.easy.exercice1;
+
+public class PositionImproved {
+
+  private final int x;
+  private final int y;
+
+  public PositionImproved(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public int getX() {
+    return x;
+  }
+}
