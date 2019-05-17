9fc8498de4f58c0497712a0ee99149a692f6fe9b
xinri <xinri@xinri.familyds.net>
2019-05-17 13:30:50 +0200

    add new exercice on the charAt

---
+++ src/test/java/com/base/easy/exercice4/MyStringUtilsShould.java
@@ -0,0 +1,32 @@
+package com.base.easy.exercice4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class MyStringUtilsShould {
+
+  @Test
+  public void return_minus_1_when_the_second_letter_of_aaa_is_not_a_Integer() {
+    // given
+    MyStringUtils myStringUtils = new MyStringUtils();
+
+    // when
+    Integer result = myStringUtils.getIntegerAt("aaa", 1);
+
+    // then
+    assertThat(result).isEqualTo(-1);
+  }
+
+  @Test
+  public void return_5_when_retrieve_the_second_letter_of_a5b_and_cast_into_Integer() {
+    // given
+    MyStringUtils myStringUtils = new MyStringUtils();
+
+    // when
+    Integer result = myStringUtils.getIntegerAt("a5b", 1);
+
+    // then
+    assertThat(result).isEqualTo(5);
+  }
+}
